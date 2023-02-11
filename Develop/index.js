92208d8bc5823e9ab04bd153eeab36b8ac08f9be
Jared Tichacek <jared.tichacek@gmail.com>
2023-02-10 20:13:46 -0600

    added function to initialize app based on user input

--- Develop/index.js
+++ Develop/index.js
@@ -64,10 +64,16 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions => {
+        fs.writeFileSync("README.md", generateMarkdown(questions))
+    })
+}
+
 
 // Function call to initialize app
 init();
